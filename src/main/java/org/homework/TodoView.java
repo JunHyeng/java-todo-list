b30295ab6eda65faf9c3b53e13ef4279dd1d6419
DESKTOP-MST6EOC\ksna0303 <ksna0303@gmail.com>
2024-06-20 00:17:28 +0900

    feat/todoview

---
+++ src/main/java/org/homework/TodoView.java
@@ -0,0 +1,45 @@
+package org.homework;
+
+import java.util.Scanner;
+
+public class TodoView {
+    private Scanner sc;
+
+    // 초기화
+    public TodoView() {
+        sc = new Scanner(System.in);
+    }
+
+    public void TodoStart() {
+        while(true) {
+            // 옵션 출력
+            System.out.println("옵션을 선택하세요 : 1.추가, 2.삭제, 3.조회, 4.삭제");
+            int option = sc.nextInt();
+            sc.nextLine();
+
+            switch (option) {
+                case 1:
+                    System.out.println("추가할 할 일을 입력하세요 : ");
+                    String desc = sc.nextLine(); // 할 일 내용 입력
+                    System.out.println(); // 할 일 추가 및 결과 출력
+                    break;
+                case 2:
+                    System.out.println("삭제할 할 일의 ID(고유번호)를 입력하세요 : ");
+                    int deleteId = sc.nextInt(); // 삭제할 ID 입력
+                    System.out.println(); // 할 일 삭제 및 결과 출력
+                    break;
+                case 3:
+                    System.out.println("조회할 할 일의 ID(고유번호)를 입력하세요 : ");
+                    int viewId = sc.nextInt(); // 조회할 ID 입력
+                    System.out.println(); // 할 일 조회 및 결과 출력
+                    break;
+                case 4:
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
+                default:
+                    System.out.println("잘못 입력하셨습니다!!!");
+                    break;
+            }
+        }
+    }
+}
